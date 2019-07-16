768e79edbcf46ecef2a59b360a11393fe6a848cd
Rick Zhou <rinick@gmail.com>
2019-07-16 16:30:17 -0700

    fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {DSLink, RootNode, ValueNode, Permission} = require("dslink");
 class MyValueNode extends ValueNode {
   constructor(path, provider) {
     super(path,          // pass path to base class
-      provider,          // path provider to base class
+      provider,          // pass provider to base class
       'myvalue',         // $is = myvalue
       'number',          // value type
       Permission.WRITE   // minimal permission required to set the value (optional)
