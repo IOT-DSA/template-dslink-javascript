b31b2b11d8709b82a4938df83b8ca5e05167c0ff
Rick Zhou <rinick@gmail.com>
2019-07-16 11:40:45 -0700

    update js dslink template to latest sdk

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
-var DS = require('dslink');
+const { DSLink, RootNode, ValueNode} = require("dslink");
 
-// creates a node with an action on it
-var Increment = DS.createNode({
-  onInvoke: function(columns) {
-    // get current value of the link
-    var previous = link.val('/counter');
-
-    // set new value by adding an amount to the previous amount
-    link.val('/counter', previous + parseInt(columns.amount));
+class MyValueNode extends ValueNode {
+  constructor(path, provider) {
+    super(path, provider, 'myvalue', 'number');
+    this._value = 123;
   }
-});
+}
 
-// Process the arguments and initializes the default nodes.
-var link = new DS.LinkProvider(process.argv.slice(2), 'template-javascript-', {
-  defaultNodes: {
-    // counter is a value node, it holds the value of our counter
-    counter: {
-      $type: 'int',
-      '?value': 0
-    },
-    // increment is an action node, it will increment /counter
-    // by the specified amount
-    increment: {
-      // references the increment profile, which makes this node an instance of
-      // our Increment class
-      $is: 'increment',
-      $invokable: 'write',
-      // $params is the parameters that are passed to onInvoke
-      $params: [
-        {
-          name: 'amount',
-          type: 'int',
-          default: 1
-        }
-      ]
-    }
-  },
-  // register our custom node here as a profile
-  // when we use $is with increment, it
-  // creates our Increment node
-  profiles: {
-    increment: function(path, provider) {
-      return new Increment(path, provider);
-    }
+class MyRootNode extends RootNode {
+  initialize() {
+    this.createChild('value', MyValueNode);
   }
-});
+}
 
-// Connect to the broker.
-// link.connect() returns a Promise.
-link.connect().catch(function(e) {
-  console.log(e.stack);
-});
+async function main() {
+  let link = new DSLink('mydslink', {rootNode: new MyRootNode()});
+  await link.connect();
+}
+
+main();
